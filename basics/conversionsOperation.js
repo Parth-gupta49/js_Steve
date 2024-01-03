a5f7358b1b3aff876bca866f6f2961c133d082f0
Parth Gupta <nbparthgupta4959@gmail.com>
2024-01-03 18:35:22 +0000

    datatypes and conversions

---
+++ basics/conversionsOperation.js
@@ -0,0 +1,35 @@
+// let score = 33
+// let score = "33a"
+// let score = null
+// let score;
+// let score = true;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score) 
+/*
+if the score variable stores "36" it will successfully convert it into the number
+however 
+if it stores "36a" then it wont be able to convert it however if we see the typeof the converted variable it will still show number as it has converted it but that is not the case over here, as we see the value for this it will show NaN => not a number
+if it stores "null" as the value then when we try to convert it into a number the value will be "0" and the type will be number only 
+*/
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber); // NaN => not a number as it was "36a" which we were trying to convert
+// console.log(valueInNumber); // NaN => not a number as it was undefined which we were trying to convert
+
+/*
+"33a" => NaN
+
+*/
+
+
+// let isLoggedIn = "parth"
+
+// let booleanLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanLoggedIn);
+// console.log(booleanLoggedIn);
+
+
+
