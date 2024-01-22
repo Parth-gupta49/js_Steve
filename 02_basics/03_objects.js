64f03ec0852c60f9140a2e66e8b788564710206d
Parth Gupta <nbparthgupta4959@gmail.com>
2024-01-22 23:36:31 +0000

    Objects files added

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,40 @@
+// Object.create()
+// Singleton
+
+// object literals
+
+const mySymbol = Symbol("key1")
+
+const JsUser = {
+    "name Hai":"Parth Gupta",
+    [mySymbol]:"Ye mera cheese hai",
+    age:19,
+    location:"Bangalore",
+    email: "parth@google.com",
+    isLoggedIn: false,
+    lastLoginDays:["Mon","Tues"]
+}
+// console.log(typeof JsUser);
+// console.log(JsUser.lastLoginDays);
+// console.log(JsUser["name Hai"]);
+
+// console.log(JsUser[mySymbol]);
+
+JsUser.email = "parth@chatgpt.com"
+// console.log(JsUser);
+
+// Object.freeze(JsUser) //now objects properties won't change at all
+// JsUser.email = "parth@microsoft.com"
+// console.log(JsUser);
+
+
+JsUser.greeting = function(){
+    console.log('Hello JsUser');
+}
+JsUser.greeting2 = function(){
+    console.log(`Hello Js User ${this["name Hai"]}`);
+}
+
+// console.log(JsUser.greeting); //[ Function (anonymous)]
+console.log(JsUser.greeting());
+console.log(JsUser.greeting2());
