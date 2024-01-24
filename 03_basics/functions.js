45b875fccf4803c09f5621a0a7b1ac9648053df5
Parth Gupta <nbparthgupta4959@gmail.com>
2024-01-24 10:53:59 +0530

    functions.js added

---
+++ 03_basics/functions.js
@@ -0,0 +1,11 @@
+// function capitalize(word) {
+//     word = word.toLowerCase()
+//     word = word[0].toUpperCase()+word.substring(1)
+//     return word
+// }
+
+
+// console.log(capitalize("SAM"));
+
+const str = "50px"
+console.log(Number.parseInt(str,10));
