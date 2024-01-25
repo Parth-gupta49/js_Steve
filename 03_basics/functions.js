7d1aa2ed1248a25158888833d12004765b8d5d6d
Parth-gupta49 <nbparthgupta4959@gmail.com>
2024-01-25 21:31:00 +0530

    functions.js modified

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -1,3 +1,22 @@
+/*
+function sayMyName() {
+    console.log("My name is Parth");
+
+}
+
+// sayMyName()
+
+function logInUserMessage(userFullName="steve") {
+    if (!userFullName) {
+        console.log("Please enter a username")
+        return
+    }
+    return userFullName + " just logged in "
+}
+
+console.log(logInUserMessage("Parth"))
+*/
+
 // function capitalize(word) {
 //     word = word.toLowerCase()
 //     word = word[0].toUpperCase()+word.substring(1)
@@ -9,3 +28,4 @@
 
 const str = "50px"
 console.log(Number.parseInt(str,10));
+
