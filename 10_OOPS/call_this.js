406ba195d8aa70bf053bcf3479caee202f53bc98
Parth Gupta <nbparthgupta4959@gmail.com>
2024-02-08 16:46:42 +0530

    call_this modified

--- 10_OOPS/call_this.js
+++ 10_OOPS/call_this.js
@@ -1,13 +1,39 @@
 
-function setUserName(username){
-    this.username = username
-}
+// function setUserName(username){
+//     this.username = username
+// }
 
-function createUser(username,email,password){
-    setUserName.call(this,username)
-    this.email = email
-    this.password = password
-}
+// function createUser(username,email,password){
+//     setUserName.call(this,username)
+//     this.email = email
+//     this.password = password
+// }
 
-const user1 = new createUser('Steve','steve@google.com',12345);
-console.log(user1);
+// const user1 = new createUser('Steve','steve@google.com',12345);
+// console.log(user1);
+
+
+// function outerFunction(x) {
+//     const myVar = "Steve"
+//     function innerFunction() {
+//         console.log(myVar);
+//     }
+//     // innerFunction();
+//     function innerFunction2(params) {
+        
+//     }
+// }
+// outerFunction()
+
+
+
+// function outerFun(x) {
+//     return function(y){
+//         return x + y;
+//     }
+// }
+
+// const add5 = outerFun(5);
+// const add10 = outerFun(10);
+
+// console.log(add5(10));
